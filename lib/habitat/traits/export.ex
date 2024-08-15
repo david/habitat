52ed45954e14aa4df9e3609cbd0b3e4bced55d1f
David Leal <293+david@users.noreply.github.com>
2024-08-15 19:22:40 +0100

    Export app from the right container

--- lib/habitat/traits/export.ex
+++ lib/habitat/traits/export.ex
@@ -1,12 +1,32 @@
 defmodule Habitat.Traits.Export do
   require Logger
 
-  def post_install({name, export: true} = spec, _container) do
-    Logger.info("Exporting #{name}")  
+  def post_install({name, export: true} = spec, container) do
+    Logger.info("Exporting #{name}")
 
-    System.cmd("distrobox-export", ["--delete", "--app", to_string(name)])
+    System.cmd("distrobox-host-exec", [
+      "distrobox",
+      "enter",
+      "--name",
+      container.name,
+      "--",
+      "distrobox-export",
+      "--delete",
+      "--app",
+      to_string(name)
+    ])
 
-    {_, 0} = System.cmd("distrobox-export", ["--app", to_string(name)])
+    {_, 0} =
+      System.cmd("distrobox-host-exec", [
+        "distrobox",
+        "enter",
+        "--name",
+        container.name,
+        "--",
+        "distrobox-export",
+        "--app",
+        to_string(name)
+      ])
 
     spec
   end
