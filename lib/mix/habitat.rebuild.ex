adad529c2ae0168f2d74a1a3722e434fc15e2cba
David Leal <293+david@users.noreply.github.com>
2024-08-17 18:21:01 +0100

    Add rebuild task

---
+++ lib/mix/habitat.rebuild.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Habitat.Rebuild do
+  @shortdoc "Rebuild containers"
+
+  use Mix.Task
+
+  @impl true
+  def run(args) do
+    Mix.Task.run("habitat.reset", args)
+    Mix.Task.run("habitat.configure", args)
+  end
+end
