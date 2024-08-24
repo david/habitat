51aaf509b510e07157c02e1784f35aa3aa3e5cb6
David Leal <293+david@users.noreply.github.com>
2024-08-24 11:55:57 +0100

    Add nodejs

---
+++ lib/habitat/programs/nodejs.ex
@@ -0,0 +1,23 @@
+defmodule Habitat.Programs.Nodejs do
+  use Habitat.Feature
+
+  def pre_sync(container, spec) do
+    Logger.info("Configuring nodejs")
+
+    container
+    |> put_package(:mise, "node", version: version(spec))
+    |> put_yarn(spec)
+  end
+
+  defp put_yarn(container, spec) do
+    if Keyword.get(spec, :package_manager) == :yarn do
+      put_package(container, :mise, "npm:yarn")
+    else
+      container
+    end
+  end
+
+  defp version([]), do: nil
+  defp version(v) when is_binary(v), do: v
+  defp version(opts) when is_list(opts), do: Keyword.get(opts, :version, version([]))
+end
