6cd3c0329494d06c8bcd13e33ee8844b176dc120
David Leal <293+david@users.noreply.github.com>
2024-08-25 18:14:17 +0100

    Add lsd

---
+++ lib/habitat/programs/lsd.ex
@@ -0,0 +1,11 @@
+defmodule Habitat.Programs.Lsd do
+  alias Habitat.Packages
+
+  require Logger
+
+  def pre_sync(container, _) do
+    Logger.info("Configuring lsd")
+
+    Packages.put(container, "lsd")
+  end
+end
