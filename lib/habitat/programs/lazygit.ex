90a9089703059b716f8adb6812f64b9ffc1b02a2
David Leal <293+david@users.noreply.github.com>
2024-08-25 18:14:20 +0100

    Add lazygit

---
+++ lib/habitat/programs/lazygit.ex
@@ -0,0 +1,11 @@
+defmodule Habitat.Programs.Lazygit do
+  alias Habitat.Packages
+
+  require Logger
+
+  def pre_sync(container, _) do
+    Logger.info("Configuring lazygit")
+
+    Packages.put(container, "lazygit")
+  end
+end
