a39ca9f12cd494e229827f339f480cbaa586ad7c
David Leal <293+david@users.noreply.github.com>
2024-08-25 18:14:26 +0100

    Add git-delta

---
+++ lib/habitat/programs/git_delta.ex
@@ -0,0 +1,11 @@
+defmodule Habitat.Programs.GitDelta do
+  alias Habitat.Packages
+
+  require Logger
+
+  def pre_sync(container, _) do
+    Logger.info("Configuring git-delta")
+
+    Packages.put(container, "git-delta")
+  end
+end
