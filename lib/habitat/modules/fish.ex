121f77b93f5e77e52a1adf81a71a235f34996b5b
David Leal <293+david@users.noreply.github.com>
2024-09-03 22:46:42 +0100

    [fish] Add some vi-related options

--- lib/habitat/modules/fish.ex
+++ lib/habitat/modules/fish.ex
@@ -1,15 +1,36 @@
 defmodule Habitat.Modules.Fish do
   use Habitat.Module
 
-  def pre_sync(container_id, _, _) do
+  def pre_sync(container_id, _, %{editing_mode: mode}) do
     install(container_id, "fish")
+
+    editing =
+      case mode do
+        :vi ->
+          """
+          set fish_cursor_default block
+          set fish_cursor_insert line
+
+          if [ $TERM_PROGRAM = "WezTerm" ]
+            set fish_vi_force_cursor 1
+          end
+
+          fish_vi_key_bindings
+          """
+
+        _ ->
+          ""
+      end
 
     insert(
       container_id,
       "~/.config/fish/config.fish",
       """
-        <%= profile %>
+      <%= profile %>
+
       if status is-interactive
+        #{editing}
+
         <%= interactive %>
       end
       """,
