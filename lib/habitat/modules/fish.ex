8d4b0b692f4a57f1a319f9f38c01230a370f9270
David Leal <293+david@users.noreply.github.com>
2024-09-06 22:22:16 +0100

    Rename editing_mode -> editing: :mode

--- lib/habitat/modules/fish.ex
+++ lib/habitat/modules/fish.ex
@@ -1,26 +1,10 @@
 defmodule Habitat.Modules.Fish do
   use Habitat.Module
 
-  def pre_sync(container_id, _, %{editing_mode: mode}) do
+  def pre_sync(container_id, _, blueprint) do
     install(container_id, "fish")
 
-    editing =
-      case mode do
-        :vi ->
-          """
-          set fish_cursor_default block
-          set fish_cursor_insert line
-
-          if [ $TERM_PROGRAM = "WezTerm" ]
-            set fish_vi_force_cursor 1
-          end
-
-          fish_vi_key_bindings
-          """
-
-        _ ->
-          ""
-      end
+    editing = blueprint |> get_in([:editing, :mode]) |> editing_mode()
 
     insert(
       container_id,
@@ -35,5 +19,25 @@ defmodule Habitat.Modules.Fish do
       end
       """
     )
+  end
+
+  defp editing_mode(:vi) do
+    """
+    set fish_cursor_default block
+    set fish_cursor_insert line
+
+    if [ $TERM_PROGRAM = "WezTerm" ]
+      set fish_vi_force_cursor 1
+    end
+
+    fish_vi_key_bindings
+    """
+  end
+
+  defp editing_mode(:emacs), do: ""
+  defp editing_mode(nil), do: ""
+
+  defp editing_mode(mode) do
+    raise "Unknown editing mode: #{inspect(mode)}"
   end
 end
--- lib/habitat/modules/readline.ex
+++ lib/habitat/modules/readline.ex
@@ -1,27 +1,34 @@
 defmodule Habitat.Modules.Readline do
   use Habitat.Module
 
-  def pre_sync(container_id, _, %{editing_mode: mode}) do
-    editing =
-      case mode do
-        :vi ->
-          """
-          set editing-mode vi
-          set vi-cmd-mode-string "\\1\\e[2 q\\2"
-          set vi-ins-mode-string "\\1\\e[6 q\\2"
-          """
-
-        _ ->
-          nil
-      end
-
+  def pre_sync(container_id, _, blueprint) do
     main =
       """
       $include /etc/inputrc
 
-      #{editing}
+      #{blueprint |> get_in([:editing, :mode]) |> editing_mode()}
       """
 
     insert(container_id, "~/.inputrc", main)
+  end
+
+  defp editing_mode(:vi) do
+    """
+    set editing-mode vi
+    set vi-cmd-mode-string "\\1\\e[2 q\\2"
+    set vi-ins-mode-string "\\1\\e[6 q\\2"
+    """
+  end
+
+  defp editing_mode(:emacs) do
+    """
+    set editing-mode emacs
+    """
+  end
+
+  defp editing_mode(nil), do: ""
+
+  defp editing_mode(mode) do
+    raise "Invalid editing mode: #{inspect(mode)}"
   end
 end
