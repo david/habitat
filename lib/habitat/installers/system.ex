b93f25fe673655128840f00660ff159d07d0c1b5
David Leal <293+david@users.noreply.github.com>
2024-09-22 15:32:58 +0100

    Remove code

--- lib/habitat/installers/system.ex
+++
@@ -1,7 +0,0 @@
-defmodule Habitat.Installers.System do
-  def sync(%{os: :ubuntu} = container, package, _) do
-    Habitat.PackageManager.Apt.install(container, [package])
-  end
-
-  def sync(container, _, _), do: container
-end
