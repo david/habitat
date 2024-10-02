22cf134bdfbc1f2046fb3f5fc9c637a84fdd9bd9
David Leal <293+david@users.noreply.github.com>
2024-10-02 17:15:22 +0100

    Configure neovim to be the default editor

--- lib/habitat/modules/neovim.ex
+++ lib/habitat/modules/neovim.ex
@@ -5,7 +5,31 @@ defmodule Habitat.Modules.Neovim do
     ["neovim"]
   end
 
-  def files(%{config: config}, _) do
-    [{"~/.config/nvim/init.lua", config}]
+  def files(%{config: config}, blueprint) do
+    [{"~/.config/nvim/init.lua", config}, shell_env(blueprint)]
+  end
+
+  defp shell_env(%{editor: :neovim, shell: :bash}) do
+    {"~/.bashrc",
+     env: """
+     export EDITOR=nvim
+     export VISUAL=nvim
+     """}
+  end
+
+  defp shell_env(%{editor: :neovim, shell: :fish}) do
+    {"~/.config/fish/config.fish",
+     env: """
+     set -gx EDITOR nvim
+     set -gx VISUAL nvim
+     """}
+  end
+
+  defp shell_env(%{editor: :neovim, shell: :zsh}) do
+    {"~/.zshrc",
+     env: """
+     export EDITOR=nvim
+     export VISUAL=nvim
+     """}
   end
 end
